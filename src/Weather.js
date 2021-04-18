5697133d1b3ce6089acdf2de30855cb737940387
Ronit Kapoor <ronitkapoor@Ronits-MacBook-Pro.local>
2021-04-18 02:00:56 -0400

    days of the week are visible now and so is the high weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,56 @@
 import { useState } from "react";
+  import { useEffect } from "react";
 
 const Weather = (props) => {
   const [weatherState, setWeatherState] = useState([
-    { date: getMyDay(props.myWeather.data[0].datetime) },
-    { date: getMyDay(props.myWeather.data[1].datetime) },
-    { date: getMyDay(props.myWeather.data[2].datetime) },
-    { date: getMyDay(props.myWeather.data[3].datetime) },
-    { date: getMyDay(props.myWeather.data[4].datetime) },
-    { date: getMyDay(props.myWeather.data[5].datetime) },
-    { date: getMyDay(props.myWeather.data[6].datetime) },
+    { date: getMyDay(props.myWeather.data[0].datetime), htemp: celsToFahren(props.myWeather.data[0].max_temp)},
+    { date: getMyDay(props.myWeather.data[1].datetime), htemp: celsToFahren(props.myWeather.data[1].max_temp)},
+    { date: getMyDay(props.myWeather.data[2].datetime),htemp: celsToFahren(props.myWeather.data[2].max_temp)},
+    { date: getMyDay(props.myWeather.data[3].datetime) ,htemp: celsToFahren(props.myWeather.data[3].max_temp)},
+    { date: getMyDay(props.myWeather.data[4].datetime), htemp: celsToFahren(props.myWeather.data[4].max_temp)},
+    { date: getMyDay(props.myWeather.data[5].datetime), htemp: celsToFahren(props.myWeather.data[5].max_temp)},
+    { date: getMyDay(props.myWeather.data[6].datetime) ,htemp: celsToFahren(props.myWeather.data[6].max_temp)}
   ]);
+
+
+
+  function celsToFahren(cels) {
+    cels = (9*cels+(32*5))/5;
+    return (Math.round(cels));
+  }
+
 
   function getMyDay(nums) {
     var newDay = new Date(nums);
     var realDay = newDay.getDay();
     if (realDay == 0) {
-      return "Sunday";
-    }
-    realDay = realDay + 1;
-    if (realDay == 1) {
       return "Monday";
-    } else if (realDay == 2) {
+    } else if (realDay == 1) {
       return "Tuesday";
-    } else if (realDay == 3) {
+    } else if (realDay == 2) {
       return "Wednesday";
-    } else if (realDay == 4) {
+    } else if (realDay == 3) {
       return "Thursday";
-    } else if (realDay == 5) {
+    } else if (realDay == 4) {
       return "Friday";
-    } else if (realDay == 6) {
+    } else if (realDay == 5) {
       return "Saturday";
+    } else if (realDay == 6) {
+      return "Sunday"
     }
+
+
   }
   return (
     <div className="box-background">
       <div className="weather-boxes">
-        {weatherState.map((block) => {
+        { weatherState.map((block) => {
           return (
             <div className="weather-box">
               <i id="degree" className="far fa-circle"></i>
               <i id="weathericon" className="fas fa-cloud-rain"></i>
               <div className="date">{block.date}</div>
-              {console.log(block.date)}
+              <div className="htemp">{block.htemp}</div>
             </div>
           );
         })}
